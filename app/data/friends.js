eaf63d93212f9424f2620308db3b3a7746ebbdc8
Nikolas  Haug <nikolashaug@Nikolass-MacBook-Air.local>
2018-06-14 11:43:30 -0500

    add survey and friends content

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,25 +2,39 @@
 
 var friends = [
     {
-        name: "spongebob",
+        name: "Spongebob",
         scores: [
-            1, 1, 1, 1, 5 ,3, 4, 1, 1, 2
+            2, 1, 5, 1, 1, 1, 3, 1, 4, 2
         ],
-        photo: "https://pmcvariety.files.wordpress.com/2016/05/spongebob-nickelodeon.jpg?w=1000&h=563&crop=1"
+        photo: "https://pmctvline2.files.wordpress.com/2017/05/spongebob-squarepants-renewed.jpg?w=620"
     },
     {
-        name: "patrick",
+        name: "Patrick",
         scores: [
-            2, 3, 4, 5, 4, 4, 1, 2, 1, 4
+            4, 2, 1, 2, 5, 5, 3, 4, 3, 3
         ],
-        photo: "photo"
+        photo: "http://vignette4.wikia.nocookie.net/spongebob/images/b/b7/SpongeBob_and_Gary2.jpg/revision/latest/scale-to-width-down/200?cb=20150623185832"
     },
     {
-        name: "sandy",
+        name: "Sandy",
         scores: [
-            2, 1, 3, 5 ,3 ,1, 3, 4, 5, 4
+            1, 2, 3, 2, 1, 1, 1, 3, 1, 1
         ],
-        photo: "photo"
+        photo: "https://t2.genius.com/unsafe/220x220/https%3A%2F%2Fimages.genius.com%2F9216fef68dec4dd39e37db0612719a3c.768x768x1.jpg"
+    },
+    {
+        name: "Mr. Krabs",
+        scores: [
+            2, 4, 1, 3, 1, 3, 1, 3, 4, 3
+        ],
+        photo: "http://net.archbold.k12.oh.us/ahs/web_class/fall_13/spongecharacter_driver/images/MrKrabs1.jpg"
+    },
+    {
+        name: "Squidward",
+        scores: [
+            1, 1, 3, 2, 2, 2, 4, 1, 3, 1
+        ],
+        photo: "https://vignette.wikia.nocookie.net/spongebob/images/e/e2/Squidward_Design_2.jpg/revision/latest?cb=20110911114021"
     }
 ]
 
